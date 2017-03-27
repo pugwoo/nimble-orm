f8d191a37f2a4d8984b1fd08c8d77f72a28b8417
pugwoo <pugwoo@gmail.com>
2017-03-27 17:31:42 +0800

    create table

--- src/test/resources/create-table.sql
+++ src/test/resources/create-table.sql
@@ -8,5 +8,29 @@ CREATE TABLE `t_student` (
   `name` varchar(32) DEFAULT 'DEFAULT',
   `age` tinyint(1) DEFAULT NULL,
   `intro` blob,
+  `school_id` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `t_school`;
+
+CREATE TABLE `t_school` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `t_course`;
+
+CREATE TABLE `t_course` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `student_id` int(11) DEFAULT NULL,
+  `name` varchar(32) DEFAULT NULL COMMENT '课程名称',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
