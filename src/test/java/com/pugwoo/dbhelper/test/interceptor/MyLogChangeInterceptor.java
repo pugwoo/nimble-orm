06c00a4788eb5fc0a4e28642a195c5841d9e75a4
pugwoo <pugwoo@gmail.com>
2017-10-19 15:58:26 +0800

    test

--- src/test/java/com/pugwoo/dbhelper/test/interceptor/MyLogChangeInterceptor.java
+++ src/test/java/com/pugwoo/dbhelper/test/interceptor/MyLogChangeInterceptor.java
@@ -9,17 +9,29 @@ public class MyLogChangeInterceptor extends DBHelperInterceptor {
 
 	@Override
 	public boolean beforeSelect(Class<?> clazz, String sql, Object[] args) {
-		System.out.println(">>> " + clazz.getSimpleName() + ",sql:" + sql + "\n    args:" + JSON.toJson(args));
+		System.out.println(">S> " + clazz.getSimpleName() + ",sql:" + sql + "\n    args:" + JSON.toJson(args));
 		return true;
 	}
 
 	@Override
 	public <T> List<T> afterSelect(Class<?> clazz, String sql, Object[] args, List<T> result, int count) {
-		System.out.println("<<< " + clazz.getSimpleName() + ",sql:" + sql + "\n    args:" + JSON.toJson(args)
+		System.out.println("<S< " + clazz.getSimpleName() + ",sql:" + sql + "\n    args:" + JSON.toJson(args)
 		    + "\n    total:" + count + ",size:" + result.size() + ",data:" + JSON.toJson(result));
 		return result;
 	}
 	
+	@Override
+	public <T> boolean beforeInsert(Class<?> clazz, List<T> list) {
+		System.out.println(">I> " + clazz.getSimpleName() + ",count:" + list.size() +
+				"\n    data:" + JSON.toJson(list));
+		return true;
+	}
 	
+	@Override
+	public <T> void afterInsert(Class<?> clazz, List<T> list, int affectedRows) {
+		System.out.println("<I< " + clazz.getSimpleName() + ",count:" + list.size()
+		        + ",affectedRows:" + affectedRows +
+				"\n    data:" + JSON.toJson(list));
+	}
 
 }
