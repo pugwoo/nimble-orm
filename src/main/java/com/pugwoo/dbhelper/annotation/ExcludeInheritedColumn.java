7c86ccdb0fbf80cb0c542accc9674d2ca57cb72b
pugwoo <pugwoo@gmail.com>
2018-02-25 08:34:39 +0800

    add ExcludeInheritedColumn annotation

---
+++ src/main/java/com/pugwoo/dbhelper/annotation/ExcludeInheritedColumn.java
@@ -0,0 +1,16 @@
+package com.pugwoo.dbhelper.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 该注解仅用于读操作。
+ * 注解上该注解的读取，将不再读取父类的除了key字段外的任何Column或RelatedColumn注解的字段。
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ExcludeInheritedColumn {
+
+}
