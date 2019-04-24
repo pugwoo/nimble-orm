1a5ac8eb3d73ec5ec9afc63c3ea928a30a348aa4
pugwoo <pugwoo@gmail.com>
2019-04-24 13:38:32 +0800

    TEST

--- src/test/java/com/pugwoo/dbhelper/test/vo/StudentSchoolJoinVO.java
+++ src/test/java/com/pugwoo/dbhelper/test/vo/StudentSchoolJoinVO.java
@@ -1,5 +1,6 @@
 package com.pugwoo.dbhelper.test.vo;
 
+import com.pugwoo.dbhelper.annotation.Column;
 import com.pugwoo.dbhelper.annotation.JoinLeftTable;
 import com.pugwoo.dbhelper.annotation.JoinRightTable;
 import com.pugwoo.dbhelper.annotation.JoinTable;
@@ -10,17 +11,32 @@ import com.pugwoo.dbhelper.test.entity.StudentDO;
 @JoinTable(joinType = JoinTypeEnum.LEFT_JOIN, on = "t1.school_id=t2.id")
 public class StudentSchoolJoinVO {
 
+	public static class StudentVO extends StudentDO {
+
+		// 特别注意：计算列的value和computed中的列都要加上表的别称，例如t1.
+		@Column(value = "t1.nameWithHi", computed = "CONCAT(t1.name,'hi')")
+		private String nameWithHi;
+
+		public String getNameWithHi() {
+			return nameWithHi;
+		}
+
+		public void setNameWithHi(String nameWithHi) {
+			this.nameWithHi = nameWithHi;
+		}
+	}
+
 	@JoinLeftTable
-	private StudentDO studentDO;
+	private StudentVO studentDO;
 	
 	@JoinRightTable
 	private SchoolDO schoolDO;
 
-	public StudentDO getStudentDO() {
+	public StudentVO getStudentDO() {
 		return studentDO;
 	}
 
-	public void setStudentDO(StudentDO studentDO) {
+	public void setStudentDO(StudentVO studentDO) {
 		this.studentDO = studentDO;
 	}
 
