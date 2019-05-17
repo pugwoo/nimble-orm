b390b2e6234a814d556bbdfd35257b15928e0488
pugwoo <pugwoo@gmail.com>
2019-05-17 10:39:51 +0800

    start develop 0.9.3

--- src/test/resources/create-table.sql
+++ src/test/resources/create-table.sql
@@ -1,18 +1,18 @@
 DROP TABLE IF EXISTS `t_student`;
 CREATE TABLE `t_student` (
-  `id` INT(11) NOT NULL AUTO_INCREMENT,
-  `deleted` TINYINT(4) DEFAULT NULL,
-  `create_time` DATETIME DEFAULT NULL,
-  `update_time` DATETIME DEFAULT NULL,
-  `delete_time` DATETIME DEFAULT NULL,
-  `name` VARCHAR(128) DEFAULT 'DEFAULT',
-  `age` TINYINT(1) DEFAULT NULL,
-  `intro` BLOB,
-  `school_id` INT(11) DEFAULT NULL,
-  `school_snapshot` VARCHAR(1024) DEFAULT NULL,
-  `course_snapshot` VARCHAR(1024) DEFAULT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `deleted` tinyint(4) DEFAULT NULL,
+  `create_time` datetime DEFAULT NULL,
+  `update_time` datetime DEFAULT NULL,
+  `delete_time` datetime DEFAULT NULL,
+  `name` varchar(128) DEFAULT 'DEFAULT',
+  `age` tinyint(1) DEFAULT NULL,
+  `intro` blob,
+  `school_id` int(11) DEFAULT NULL,
+  `school_snapshot` varchar(1024) DEFAULT NULL,
+  `course_snapshot` varchar(1024) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=INNODB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `t_school`;
 CREATE TABLE `t_school` (
@@ -20,7 +20,7 @@ CREATE TABLE `t_school` (
   `deleted` tinyint(4) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
-  `delete_time` DATETIME DEFAULT NULL,
+  `delete_time` datetime DEFAULT NULL,
   `name` varchar(32) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -31,7 +31,7 @@ CREATE TABLE `t_course` (
   `deleted` tinyint(4) DEFAULT NULL,
   `create_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
-  `delete_time` DATETIME DEFAULT NULL,
+  `delete_time` datetime DEFAULT NULL,
   `student_id` int(11) DEFAULT NULL,
   `name` varchar(32) DEFAULT NULL COMMENT '课程名称',
   `is_main` tinyint(4) DEFAULT '0' COMMENT '是否主课程',
@@ -45,18 +45,18 @@ CREATE TABLE `t_json` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `t_json_raw`;
+CREATE TABLE `t_json_raw` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `json` varchar(4096) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `t_cas_version`;
 CREATE TABLE `t_cas_version` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(32) DEFAULT NULL,
   `version` int(11) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
-DROP TABLE IF EXISTS `t_json_raw`;
-CREATE TABLE `t_json_raw` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `json` varchar(4096) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
