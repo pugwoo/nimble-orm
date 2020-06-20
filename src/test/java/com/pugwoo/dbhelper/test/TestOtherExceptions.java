b8172ebea825866da46acd9afc087cc19f5c54c9
pugwoo <pugwoo@gmail.com>
2020-06-20 13:47:39 +0800

    完善单元测试

---
+++ src/test/java/com/pugwoo/dbhelper/test/TestOtherExceptions.java
@@ -0,0 +1,86 @@
+package com.pugwoo.dbhelper.test;
+
+import com.pugwoo.dbhelper.DBHelper;
+import com.pugwoo.dbhelper.annotation.Column;
+import com.pugwoo.dbhelper.annotation.Table;
+import com.pugwoo.dbhelper.exception.MustProvideConstructorException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 测试各种异常的情况
+ */
+@ContextConfiguration(locations = "classpath:applicationContext-jdbc.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+@Transactional
+public class TestOtherExceptions {
+
+    @Autowired
+    private DBHelper dbHelper;
+
+    @Test
+    @Rollback(false)
+    public void deleteEx() {
+        boolean ex = false;
+        try {
+            dbHelper.deleteByKey(StudentDO.class, 1L);
+        } catch (Exception e) {
+            assert e instanceof MustProvideConstructorException;
+            ex = true;
+        }
+        assert ex;
+
+        ex = false;
+        try {
+            dbHelper.deleteByKey(StudentDO2.class, 1L);
+        } catch (Exception e) {
+            assert e instanceof MustProvideConstructorException;
+            ex = true;
+        }
+        assert ex;
+    }
+
+    @Table("t_student")
+    private static class StudentDO {
+
+        public StudentDO(Long id) { // 故意让这个DO类没有默认构造函数
+            this.id = id;
+        }
+
+        @Column(value = "id", isKey = true, isAutoIncrement = true)
+        private Long id;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+    }
+
+    @Table("t_student")
+    private static class StudentDO2 {
+
+        private StudentDO2() { // 故意让这个DO类默认构造函数是私有的
+            this.id = id;
+        }
+
+        @Column(value = "id", isKey = true, isAutoIncrement = true)
+        private Long id;
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
+    }
+
+}
