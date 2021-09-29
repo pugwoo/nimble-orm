03ae2cfbec1daad9d25c1e4e4f08c4680af98e94
pugwoo <pugwoo@gmail.com>
2021-09-29 08:14:07 +0800

    clean code

--- src/main/java/com/pugwoo/dbhelper/impl/DBHelperContext.java
+++ src/main/java/com/pugwoo/dbhelper/impl/DBHelperContext.java
@@ -9,15 +9,13 @@ import java.util.Map;
 public class DBHelperContext {
 
     /**自定义表名，适合于分表场景*/
-    private static ThreadLocal<Map<Class<?>, String>> _tableNames = new ThreadLocal<Map<Class<?>, String>>();
+    private static final ThreadLocal<Map<Class<?>, String>> _tableNames = new ThreadLocal<>();
 
     /**关闭软删除*/
-    private static ThreadLocal<Map<Class<?>, Boolean>> _turnOffSoftDelete = new ThreadLocal<Map<Class<?>, Boolean>>();
+    private static final ThreadLocal<Map<Class<?>, Boolean>> _turnOffSoftDelete = new ThreadLocal<>();
 
     /**
      * 获得类对应的自定义表名，不存在返回null
-     * @param clazz
-     * @return
      */
     public static String getTableName(Class<?> clazz) {
         Map<Class<?>, String> tableNames = _tableNames.get();
@@ -25,16 +23,11 @@ public class DBHelperContext {
             return null;
         }
 
-        String tableName = tableNames.get(clazz);
-        if(tableName == null) {
-            return null;
-        }
-        return tableName;
+        return tableNames.get(clazz);
     }
 
     /**
      * 设置类对应的自定义表名
-     * @param clazz
      * @param tableName 不需要加反引号`
      */
     public static void setTableName(Class<?> clazz, String tableName) {
@@ -48,7 +41,7 @@ public class DBHelperContext {
 
         Map<Class<?>, String> tableNames = _tableNames.get();
         if(tableNames == null) {
-            tableNames = new HashMap<Class<?>, String>();
+            tableNames = new HashMap<>();
             _tableNames.set(tableNames);
         }
         tableNames.put(clazz, tableName);
@@ -63,8 +56,6 @@ public class DBHelperContext {
 
     /**
      * 查询指定类是否关闭了软删除
-     * @param clazz
-     * @return
      */
     public static boolean isTurnOffSoftDelete(Class<?> clazz) {
         if (clazz == null) {
@@ -81,7 +72,6 @@ public class DBHelperContext {
 
     /**
      * 关闭指定类的软删除设置，仅对当前线程有效
-     * @param clazz
      */
     public static void turnOffSoftDelete(Class<?> clazz) {
         if (clazz == null) {
@@ -89,7 +79,7 @@ public class DBHelperContext {
         }
         Map<Class<?>, Boolean> turnoff = _turnOffSoftDelete.get();
         if(turnoff == null) {
-            turnoff = new HashMap<Class<?>, Boolean>();
+            turnoff = new HashMap<>();
             _turnOffSoftDelete.set(turnoff);
         }
         turnoff.put(clazz, true);
@@ -97,7 +87,6 @@ public class DBHelperContext {
 
     /**
      * 打开指定类的软删除设置
-     * @param clazz
      */
     public static void turnOnSoftDelete(Class<?> clazz) {
         if (clazz == null) {
