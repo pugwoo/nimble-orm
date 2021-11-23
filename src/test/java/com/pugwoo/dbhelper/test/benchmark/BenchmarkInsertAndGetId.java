d276c9d3fd80cd9ddaacbea4024ef6287587ee32
pugwoo <pugwoo@gmail.com>
2021-11-23 23:42:37 +0800

    clean code

--- src/test/java/com/pugwoo/dbhelper/test/benchmark/BenchmarkInsertAndGetId.java
+++ src/test/java/com/pugwoo/dbhelper/test/benchmark/BenchmarkInsertAndGetId.java
@@ -20,6 +20,11 @@ public class BenchmarkInsertAndGetId {
     @Autowired
     private DBHelper dbHelper;
 
+    /**
+     * 该测试用于测试获取自增ID的正确性。
+     * 测试方式：起400个并发线程，不停地插入1万条数据并获取自增id，通过自增id再去查回数据比较，从而校验其正确性。
+     * 结论：400万个均正确。
+     */
     @Test
     public void benchmark() {
 
@@ -79,72 +84,6 @@ public class BenchmarkInsertAndGetId {
         executeThem.waitAllTerminate();
         System.out.println("insert:" + insert.get() + ",insertFail:" + insertFail.get()
            + ",getIdFail:" + getIdFail.get());
-
-        assert insert.get() > 0;
-        assert insertFail.get() == 0;
-        assert getIdFail.get() == 0;
-    }
-
-    @Test
-    public void benchmarkWhereNotExist() {
-
-        final AtomicLong insert = new AtomicLong(0);
-        final AtomicLong insertFail = new AtomicLong(0);
-        final AtomicLong getIdFail = new AtomicLong(0);
-
-        Thread t = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                while(true) {
-                    System.out.println("insert:" + insert.get() + ",insertFail:" + insertFail.get()
-                            + ",getIdFail:" + getIdFail.get());
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-        });
-        t.setDaemon(true);
-        t.start();
-
-        // 起400个线程
-        ExecuteThem executeThem = new ExecuteThem(400);
-
-        for (int i = 0; i < 400; i++) {
-            executeThem.add(new Runnable() {
-                @Override
-                public void run() {
-                    for (int i = 0; i < 10000; i++) {
-                        StudentDO studentDO = new StudentDO();
-
-                        String name = UUID.randomUUID().toString().replace("-", "");
-                        studentDO.setName(name);
-
-                        int row = dbHelper.insertWhereNotExist(studentDO, "where name=?", name);
-                        if (row != 1) {
-                            insertFail.incrementAndGet();
-                        } else {
-                            insert.incrementAndGet();
-                        }
-
-                        StudentDO std2 = dbHelper.getOne(StudentDO.class, "where id=?", studentDO.getId());
-                        if(std2 == null) {
-                            System.err.println("id:" + studentDO.getId() + " not exist in db");
-                            getIdFail.incrementAndGet();
-                        } else if (!name.equals(std2.getName())) {
-                            System.err.println("id:" + studentDO.getId() + " name not match, orgin:"
-                               + name + ",in fact:" + std2.getName());
-                            getIdFail.incrementAndGet();
-                        }
-                    }
-                }
-            });
-        }
-
-        executeThem.waitAllTerminate();
-        System.out.println("insert:" + insert.get() + ",insertFail:" + insertFail.get()
-                + ",getIdFail:" + getIdFail.get());
 
         assert insert.get() > 0;
         assert insertFail.get() == 0;
