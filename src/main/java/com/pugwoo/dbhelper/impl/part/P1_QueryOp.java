40bc51d51487e5de6f62b1944436463ce4506229
pugwoo <pugwoo@gmail.com>
2022-04-27 08:33:57 +0800

    自动移除getOne和getPage时，postSql参数中的limit子句

--- src/main/java/com/pugwoo/dbhelper/impl/part/P1_QueryOp.java
+++ src/main/java/com/pugwoo/dbhelper/impl/part/P1_QueryOp.java
@@ -467,6 +467,10 @@ public abstract class P1_QueryOp extends P0_JdbcTemplateOp {
 
         StringBuilder sql = new StringBuilder();
         sql.append(SQLUtils.getSelectSQL(clazz, selectOnlyKey, false, features, postSql));
+        // 当limit不为null时，分页由orm内部控制，此时postSql不应该包含limit子句，这里尝试去除
+        if (limit != null) {
+            postSql = SQLUtils.removeLimit(postSql);
+        }
         sql.append(SQLUtils.autoSetSoftDeleted(postSql, clazz));
         sql.append(SQLUtils.genLimitSQL(offset, limit));
 
