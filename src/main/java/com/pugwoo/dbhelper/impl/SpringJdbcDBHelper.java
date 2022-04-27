3867813e23fe9858bd5acbdc26bd3fe6359e1b0e
pugwoo <pugwoo@gmail.com>
2022-04-27 08:32:34 +0800

    添加接收JdbcTemplate的DBHelper构造函数

--- src/main/java/com/pugwoo/dbhelper/impl/SpringJdbcDBHelper.java
+++ src/main/java/com/pugwoo/dbhelper/impl/SpringJdbcDBHelper.java
@@ -1,6 +1,7 @@
 package com.pugwoo.dbhelper.impl;
 
 import com.pugwoo.dbhelper.impl.part.P5_DeleteOp;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 /**
  * 2015年1月12日 16:41:03 数据库操作封装：增删改查
@@ -9,5 +10,12 @@ import com.pugwoo.dbhelper.impl.part.P5_DeleteOp;
 public class SpringJdbcDBHelper extends P5_DeleteOp {
 	
 	// 实现分别安排在impl.part包下几个文件中
+
+    public SpringJdbcDBHelper() {
+    }
+
+    public SpringJdbcDBHelper(JdbcTemplate jdbcTemplate) {
+        this.setJdbcTemplate(jdbcTemplate);
+    }
 
 }
