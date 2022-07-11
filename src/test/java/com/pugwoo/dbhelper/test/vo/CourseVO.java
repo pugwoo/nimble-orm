475956e5c0f767a8be62684f5f476a47bc19fb2c
pugwoo <pugwoo@gmail.com>
2022-07-11 23:08:58 +0800

    单元测试

--- src/test/java/com/pugwoo/dbhelper/test/vo/CourseVO.java
+++ src/test/java/com/pugwoo/dbhelper/test/vo/CourseVO.java
@@ -13,6 +13,11 @@ public class CourseVO extends CourseDO {
             conditional = "t.getIsMain() != null && t.getIsMain()")
     private List<StudentDO> mainCourseStudents;
 
+    // 这里使用conditional，对只有是主课程的学生才进行查询
+    @RelatedColumn(localColumn = "student_id", remoteColumn = "id",
+            conditional = "t.getIsMain() != null && t.getIsMain()")
+    private StudentDO mainCourseStudent;
+
     public List<StudentDO> getMainCourseStudents() {
         return mainCourseStudents;
     }
@@ -21,4 +26,11 @@ public class CourseVO extends CourseDO {
         this.mainCourseStudents = mainCourseStudents;
     }
 
+    public StudentDO getMainCourseStudent() {
+        return mainCourseStudent;
+    }
+
+    public void setMainCourseStudent(StudentDO mainCourseStudent) {
+        this.mainCourseStudent = mainCourseStudent;
+    }
 }
