7ef9558182d341b0e2c7f98391bfd9c4f957cc09
pugwoo <pugwoo@gmail.com>
2022-12-12 22:22:13 +0800

    benchmark and ex tests

---
+++ nimble-orm-test-mysql/src/test/java/com/pugwoo/dbhelper/test/benchmark/BenchmarkInsertAndGetId.java
@@ -0,0 +1,93 @@
+package com.pugwoo.dbhelper.test.benchmark;
+
+import com.pugwoo.dbhelper.DBHelper;
+import com.pugwoo.dbhelper.test.entity.StudentDO;
+import com.pugwoo.wooutils.task.ExecuteThem;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
+
+@SpringBootTest
+// 这里不能用@Transactional，就是要测没有事务情况下的
+public class BenchmarkInsertAndGetId {
+
+    @Autowired
+    private DBHelper dbHelper;
+
+    /**
+     * 该测试用于测试获取自增ID的正确性。
+     * 测试方式：起400个并发线程，不停地插入1万条数据并获取自增id，通过自增id再去查回数据比较，从而校验其正确性。
+     * 结论：400万个均正确。
+     */
+    @Test
+    @EnabledIfSystemProperty(named = "runBenchmarkTest", matches = "true")
+    public void benchmark() {
+
+        final AtomicLong insert = new AtomicLong(0);
+        final AtomicLong insertFail = new AtomicLong(0);
+        final AtomicLong getIdFail = new AtomicLong(0);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while(true) {
+                    System.out.println("insert:" + insert.get() + ",insertFail:" + insertFail.get()
+                            + ",getIdFail:" + getIdFail.get());
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                    }
+                }
+            }
+        });
+        t.setDaemon(true);
+        t.start();
+
+        // 起400个线程
+        ExecuteThem executeThem = new ExecuteThem(400);
+
+        for (int i = 0; i < 400; i++) {
+            executeThem.add(new Runnable() {
+                @Override
+                public void run() {
+                    for (int i = 0; i < 10000; i++) {
+                        StudentDO studentDO = new StudentDO();
+
+                        String name = UUID.randomUUID().toString().replace("-", "");
+                        studentDO.setName(name);
+
+                        int row = dbHelper.insert(studentDO);
+                        if (row != 1) {
+                            insertFail.incrementAndGet();
+                        } else {
+                            insert.incrementAndGet();
+                        }
+
+                        StudentDO std2 = dbHelper.getOne(StudentDO.class, "where id=?", studentDO.getId());
+                        if(std2 == null) {
+                            System.err.println("id:" + studentDO.getId() + " not exist in db");
+                            getIdFail.incrementAndGet();
+                        } else if (!name.equals(std2.getName())) {
+                            System.err.println("id:" + studentDO.getId() + " name not match");
+                            getIdFail.incrementAndGet();
+                        }
+                    }
+                }
+            });
+        }
+
+        executeThem.waitAllTerminate();
+        System.out.println("insert:" + insert.get() + ",insertFail:" + insertFail.get()
+           + ",getIdFail:" + getIdFail.get());
+
+        assert insert.get() > 0;
+        assert insertFail.get() == 0;
+        assert getIdFail.get() == 0;
+    }
+
+}
