a00955dff9d4684e741314a9b06957596e06bfc2
pugwoo <pugwoo@gmail.com>
2023-02-20 22:01:36 +0800

    RowMapperFailException时log字段信息

--- nimble-orm/src/main/java/com/pugwoo/dbhelper/exception/RowMapperFailException.java
+++ nimble-orm/src/main/java/com/pugwoo/dbhelper/exception/RowMapperFailException.java
@@ -1,11 +1,16 @@
 package com.pugwoo.dbhelper.exception;
 
+import java.lang.reflect.Field;
+
 /**
  * 数据行转换成Java对象时失败异常
  */
 public class RowMapperFailException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
+
+	// 处理失败的field
+	private Field field;
 	
 	public RowMapperFailException() {
 	}
@@ -17,4 +22,14 @@ public class RowMapperFailException extends RuntimeException {
 	public RowMapperFailException(Throwable e) {
 		super(e);
 	}
+
+	public RowMapperFailException(Throwable e, Field field) {
+		super(e);
+		this.field = field;
+	}
+
+	public Field getField() {
+		return field;
+	}
+
 }
