682f081f3cb80e0b483a8c3b2dcbf1e5053b04e3
pugwoo <pugwoo@gmail.com>
2023-04-01 10:13:08 +0800

    update单元测试

--- nimble-orm-test/src/test/java/com/pugwoo/dbhelper/test/test_common/Test2Update_Batch.java
+++ nimble-orm-test/src/test/java/com/pugwoo/dbhelper/test/test_common/Test2Update_Batch.java
@@ -1,13 +1,16 @@
 package com.pugwoo.dbhelper.test.test_common;
 
 import com.pugwoo.dbhelper.DBHelper;
+import com.pugwoo.dbhelper.test.entity.IdableSoftDeleteBaseDO;
 import com.pugwoo.dbhelper.test.entity.StudentDO;
 import com.pugwoo.dbhelper.test.utils.CommonOps;
+import com.pugwoo.wooutils.collect.ListUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
+import java.util.Map;
 
 /**
  * 测试批量更新
@@ -21,6 +24,8 @@ public class Test2Update_Batch {
     @Test
     public void testUpdateBatch() {
         List<StudentDO> list = CommonOps.insertBatch(dbHelper, 11);
+        List<Long> ids = ListUtils.transform(list, IdableSoftDeleteBaseDO::getId);
+        Map<Long, StudentDO> map = ListUtils.toMap(list, IdableSoftDeleteBaseDO::getId, o -> o);
 
         for (StudentDO studentDO : list) {
             studentDO.setName(studentDO.getName() + "x");
@@ -28,5 +33,11 @@ public class Test2Update_Batch {
 
         int rows = dbHelper.update(list);
         assert rows == list.size();
+
+        List<StudentDO> all = dbHelper.getAll(StudentDO.class, "where id in (?)", ids);
+        assert all.size() == list.size();
+        for (StudentDO studentDO : all) { // 验证修改确实成功
+            assert studentDO.getName().equals(map.get(studentDO.getId()).getName());
+        }
     }
 }
