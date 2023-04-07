06124722e8438d48d009dd8694c14c35a17a0ee0
pugwoo <pugwoo@gmail.com>
2023-04-07 23:15:11 +0800

    完善单元测试

--- nimble-orm-test/src/test/java/com/pugwoo/dbhelper/test/test_common/Test2Update_Batch.java
+++ nimble-orm-test/src/test/java/com/pugwoo/dbhelper/test/test_common/Test2Update_Batch.java
@@ -1,12 +1,16 @@
 package com.pugwoo.dbhelper.test.test_common;
 
 import com.pugwoo.dbhelper.DBHelper;
+import com.pugwoo.dbhelper.annotation.Column;
+import com.pugwoo.dbhelper.annotation.Table;
 import com.pugwoo.dbhelper.exception.CasVersionNotMatchException;
 import com.pugwoo.dbhelper.test.entity.CasVersionDO;
 import com.pugwoo.dbhelper.test.entity.IdableSoftDeleteBaseDO;
+import com.pugwoo.dbhelper.test.entity.SchoolDO;
 import com.pugwoo.dbhelper.test.entity.StudentDO;
 import com.pugwoo.dbhelper.test.utils.CommonOps;
 import com.pugwoo.wooutils.collect.ListUtils;
+import lombok.Data;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -214,4 +218,104 @@ public class Test2Update_Batch {
 
     }
 
+    @Test
+    public void testUpdateDifferentDO() {
+        List<Object> list = new ArrayList<>();
+
+        StudentDO studentDO = new StudentDO();
+        studentDO.setName("test");
+        dbHelper.insert(studentDO);
+        list.add(studentDO);
+
+        SchoolDO schoolDO = new SchoolDO();
+        schoolDO.setName("sysu");
+        dbHelper.insert(schoolDO);
+        list.add(schoolDO);
+
+        // 修改name
+        studentDO.setName(UUID.randomUUID().toString().replace("-", "").substring(0, 16));
+        schoolDO.setName(UUID.randomUUID().toString().replace("-", "").substring(0, 16));
+
+        assert dbHelper.update(list) == 2;
+
+        // 查回来检查数据
+        StudentDO one = dbHelper.getOne(StudentDO.class, "where id=?", studentDO.getId());
+        assert one.getName().equals(studentDO.getName());
+
+        SchoolDO b = dbHelper.getOne(SchoolDO.class, "where id=?", schoolDO.getId());
+        assert b.getName().equals(schoolDO.getName());
+
+    }
+
+    @Test
+    public void testSomeExCase() {
+        List<StudentDO> studentDOS = CommonOps.insertBatch(dbHelper, 3);
+
+        // 测试没有非主键的DO
+        List<StudentOnlyKeyDO> list = new ArrayList<>();
+        for (int i = 0; i < studentDOS.size(); i++) {
+            StudentOnlyKeyDO student = new StudentOnlyKeyDO();
+            student.setId(studentDOS.get(i).getId());
+            list.add(student);
+        }
+        assert dbHelper.update(list) == 0; // 只有主键，所以不会有任何更新，即使id存在
+
+
+        // 测试多个casVersion注解的情况，此时会抛异常
+        List<MultiCasVersionDO> list2 = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            MultiCasVersionDO d = new MultiCasVersionDO();
+            d.setId(i + 1L);
+            list2.add(d);
+        }
+        boolean isThrow = false;
+        try {
+            dbHelper.update(list2);
+        } catch (Exception e) {
+            isThrow = true;
+            assert e.getMessage().contains("has more than one casVersion column");
+        }
+        assert isThrow;
+
+        // 测试更新student，但是传入的非key值都是null，等于不需要更新
+        List<StudentKeyAndNameDO> list3 = new ArrayList<>();
+        for (StudentDO studentDO : studentDOS) {
+            StudentKeyAndNameDO d = new StudentKeyAndNameDO();
+            d.setId(studentDO.getId());
+            list3.add(d);
+        }
+        assert dbHelper.update(list3) == 0; // 所有非主键列都是null
+    }
+
+    @Data
+    @Table("t_student")
+    public static class StudentOnlyKeyDO {
+        @Column(value = "id", isKey = true, isAutoIncrement = true)
+        private Long id;
+    }
+
+    @Data
+    @Table("t_student")
+    public static class StudentKeyAndNameDO {
+        @Column(value = "id", isKey = true, isAutoIncrement = true)
+        private Long id;
+
+        @Column(value = "name")
+        private String name;
+    }
+
+    @Data
+    @Table("t_student")
+    public static class MultiCasVersionDO {
+
+        @Column(value = "id", isKey = true, isAutoIncrement = true)
+        private Long id;
+
+        @Column(value = "age", casVersion = true)
+        private Integer age;
+
+        @Column(value = "school_id", casVersion = true)
+        private Long schoolId;
+
+    }
 }
