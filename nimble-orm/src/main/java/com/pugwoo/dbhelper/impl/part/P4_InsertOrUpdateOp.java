9424bbcae41a1be95b5bad9b75142f33c940e091
pugwoo <pugwoo@gmail.com>
2023-04-11 23:54:14 +0800

    优化insertOrUpdate方法，支持批量，提高性能

--- nimble-orm/src/main/java/com/pugwoo/dbhelper/impl/part/P4_InsertOrUpdateOp.java
+++ nimble-orm/src/main/java/com/pugwoo/dbhelper/impl/part/P4_InsertOrUpdateOp.java
@@ -1,8 +1,10 @@
 package com.pugwoo.dbhelper.impl.part;
 
 import com.pugwoo.dbhelper.utils.DOInfoReader;
+import com.pugwoo.dbhelper.utils.InnerCommonUtils;
 
 import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -10,11 +12,11 @@ public abstract class P4_InsertOrUpdateOp extends P3_UpdateOp {
 
 	@Override
 	public <T> int insertOrUpdate(T t) {
-		if(t == null) {
+		if (t == null) {
 			return 0;
 		}
 
-		if(isWithKey(t)) {
+		if (isWithKey(t)) {
 			return update(t);
 		} else {
 			return insert(t);
@@ -23,11 +25,11 @@ public abstract class P4_InsertOrUpdateOp extends P3_UpdateOp {
 	
 	@Override
 	public <T> int insertOrUpdateWithNull(T t) {
-		if(t == null) {
+		if (t == null) {
 			return 0;
 		}
 
-		if(isWithKey(t)) {
+		if (isWithKey(t)) {
 			return updateWithNull(t);
 		} else {
 			return insertWithNull(t);
@@ -36,15 +38,27 @@ public abstract class P4_InsertOrUpdateOp extends P3_UpdateOp {
 	
 	@Override
 	public <T> int insertOrUpdate(Collection<T> list) {
-		if(list == null || list.isEmpty()) {
+		if (InnerCommonUtils.isEmpty(list)) {
 			return 0;
 		}
-		int rows = 0;
+
+		// 将insert和update拆开进行调用，更好的利用批量能力
+		List<T> toInsert = new ArrayList<>();
+		List<T> toUpdate = new ArrayList<>();
+
 		for(T t : list) {
 			if(t != null) {
-				rows += insertOrUpdate(t);
+				if(isWithKey(t)) {
+					toUpdate.add(t);
+				} else {
+					toInsert.add(t);
+				}
 			}
 		}
+
+		int rows = 0;
+		rows += insert(toInsert);
+		rows += update(toUpdate);
 		return rows;
 	}
 
