2e82e44142dcafc9b6888638c3a1f44fd2577301
pugwoo <pugwoo@gmail.com>
2023-04-27 23:43:38 +0800

    add test table

--- nimble-orm-test/sql/create-table-mysql.sql
+++ nimble-orm-test/sql/create-table-mysql.sql
@@ -14,6 +14,22 @@ CREATE TABLE `t_student` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `t_student_del`;
+CREATE TABLE `t_student_del` (
+   `id` int(11) NOT NULL AUTO_INCREMENT,
+   `deleted` int(11) DEFAULT NULL,
+   `create_time` datetime DEFAULT NULL,
+   `update_time` datetime DEFAULT NULL,
+   `delete_time` datetime DEFAULT NULL,
+   `name` varchar(128) DEFAULT 'DEFAULT',
+   `age` tinyint(1) DEFAULT NULL,
+   `intro` blob,
+   `school_id` int(11) DEFAULT NULL,
+   `school_snapshot` varchar(1024) DEFAULT NULL,
+   `course_snapshot` varchar(1024) DEFAULT NULL,
+   PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `t_school`;
 CREATE TABLE `t_school` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
